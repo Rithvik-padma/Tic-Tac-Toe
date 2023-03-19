a1a1e6e5b554297b4a239ad0450d9bebe005a8e4
Rithvik-padma <99183441+Rithvik-padma@users.noreply.github.com>
2023-03-19 23:23:45 +0530

    Main page complete

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="home">
+    <p class="home_title">X's and O's</p>
+    <p class="home_head">It's time to show!</p>
+    <div class="home_wrap">
+      <img class="home_image" src="../assets/tic-tac-toe.svg" />
+    </div>
+    <router-link :to="{name: 'game'}">
+      <button class="home_play">Start a new game!</button>
+    </router-link>
   </div>
 </template>
 
@@ -8,3 +16,6 @@ export default {
   name: 'HomeView',
 }
 </script>
+<style lang="stylus" scoped>
+@import '../styles/home.styl'
+</style>
