ce1db65dd19a63e7755a52f29ece4d36d2b4990d
Rithvik-padma <99183441+Rithvik-padma@users.noreply.github.com>
2023-03-28 00:16:01 +0530

    Add the design to the Game view

---
+++ src/components/GameHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1 class="gameView_title">{{ header }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'GameHeader',
+        data(){
+            return {
+            }
+        },
+        props: {
+            header: String
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    @import '../styles/game.styl'
+</style>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,15 +1,34 @@
 <template>
-  <div>
-
+  <div class="gameView">
+    <GameHeader :header="header"/>
+    <TicTacToe :squares="squares" :player="player"/>
+    <router-link :to="{name:GameView}">
+      <button v-show="complete" class="gameView_reset">Reset</button>
+    </router-link>
   </div>
 </template>
 
 <script>
+import GameHeader from '../components/GameHeader.vue'
+import TicTacToe from '../components/TicTacToe.vue'
+
   export default {
-    name: 'GameView'
+    name: 'GameView',
+    components:{
+      GameHeader,
+      TicTacToe
+    },
+    data(){
+      return {
+        squares: ['','','','','','','','',''],
+        header: "Player X's turn",
+        player: "x",
+        complete: true
+      }
+    }
   }
 </script>
 
 <style lang="stylus" scoped>
-
+  @import '../styles/game.styl'
 </style>
